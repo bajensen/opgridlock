85cd7f623116ce1cf9f56220d637c9d2debf7a7a
Brendon Jensen <brendonjensen@gmail.com>
2017-09-20 21:29:53 -0600

    More meaningful states.

--- bin/latch_node.py
+++ bin/latch_node.py
@@ -15,7 +15,7 @@ else:
 def on_door_state(door_open):
     print('Door changed state: ' + str(door_open))
 
-    client.publish(config.get('MQTT', 'mqtt_door_state'), door_open, 'open' if True else 'closed')
+    client.publish(config.get('MQTT', 'mqtt_door_state'), 'open' if True else 'closed', 0, True)
 
 
 def on_connect(client, user_data, flags, result_code):
